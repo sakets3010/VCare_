f11b173d2e6e794852831942299438e80e70c545
sakets3010 <sakets3010@gmail.com>
2020-06-21 12:37:22 +0530

    added sign-in checks and profile holder

--- app/src/main/java/com/example/vcare/Login_Sign_in_fragment.kt
+++ app/src/main/java/com/example/vcare/Login_Sign_in_fragment.kt
@@ -1,21 +1,104 @@
 package com.example.vcare
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.vcare.databinding.FragmentLoginSignInFragmentBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class Login_Sign_in_fragment : Fragment() {
+    val RC_SIGN_IN: Int = 1
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var mGoogleSignInOptions: GoogleSignInOptions
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: FragmentLoginSignInFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        firebaseAuth = FirebaseAuth.getInstance()
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login__sign_in_fragment,container,false)
+        configureGoogleSignIn()
+        setupUI()
+
+
+
+
+
         return binding.root
     }
+
+    private fun configureGoogleSignIn() {
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireContext(),mGoogleSignInOptions)
+    }
+    private fun setupUI() {
+        binding.googleButton.setOnClickListener {
+            signIn()
+        }
+    }
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            } catch (e: ApiException) {
+                Toast.makeText(requireContext(), "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Navigation.findNavController(requireActivity(),R.id.login_navhost).navigate(R.id.action_login_Sign_in_fragment_to_login_enter_detail_fragment)
+                Toast.makeText(requireContext(),"Sign in sucessfull!",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            Navigation.findNavController(requireActivity(),R.id.login_navhost).navigate(R.id.action_login_Sign_in_fragment_to_login_enter_detail_fragment)
+        }
+
+    }
+    //for opening the login activity through an intent.
+    companion object {
+        fun getLaunchIntent(from: Context) = Intent(from, LoginActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/vcare/Login_enter_detail_fragment.kt
+++ app/src/main/java/com/example/vcare/Login_enter_detail_fragment.kt
@@ -1,12 +1,25 @@
 package com.example.vcare
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.vcare.databinding.FragmentLoginEnterDetailFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Login_enter_detail_fragment : Fragment() {
@@ -15,9 +28,49 @@ class Login_enter_detail_fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login_enter_detail_fragment,container,false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login_enter_detail_fragment,
+            container,
+            false
+        )
+        setupUI()
+        binding.selectPhotoButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+
         return binding.root
+    }
+    var selectedPhotoUri:Uri?=null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+             selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,selectedPhotoUri)
+            binding.selectPhotoButton.setBackgroundDrawable(BitmapDrawable(bitmap))
+        }
+    }
+
+    private fun setupUI() {
+        binding.signOut.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        Navigation.findNavController(requireActivity(), R.id.login_navhost)
+            .navigate(R.id.action_login_enter_detail_fragment_to_login_Sign_in_fragment)
+        Toast.makeText(requireContext(), "Sign out successful!", Toast.LENGTH_SHORT).show()
+        FirebaseAuth.getInstance().signOut()
     }
 
 
-}
+
+    }
+
+
+
+
